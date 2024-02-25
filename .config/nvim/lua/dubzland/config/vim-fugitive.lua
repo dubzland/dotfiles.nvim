b48d95db70d355bc0999bd26855e73f88296e6a3
Josh Williams <jdubz@dubzland.com>
2024-02-25 09:47:39 -0600

    Update fugitive keymaps

--- .config/nvim/lua/dubzland/config/vim-fugitive.lua
+++ .config/nvim/lua/dubzland/config/vim-fugitive.lua
@@ -4,7 +4,7 @@ local fugitive_config = {}
 
 fugitive_config.init = function()
     utils.keys.nnoremap("<leader>gs", vim.cmd.Git, "Show git status window")
-    utils.keys.nnoremap("<leader>gc", ":Git commit", "Open the git commit window")
+    utils.keys.nnoremap("<leader>gc", function() vim.cmd.Git("commit") end, "Open the git commit window")
 end
 
 return fugitive_config

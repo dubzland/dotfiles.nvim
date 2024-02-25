37e8adcaa68bb32c8b49658d7af59106f408cc5a
Josh Williams <jdubz@dubzland.com>
2024-02-25 12:57:08 -0600

    Add vertical git diff mapping

--- .config/nvim/lua/dubzland/config/vim-fugitive.lua
+++ .config/nvim/lua/dubzland/config/vim-fugitive.lua
@@ -3,8 +3,10 @@ local utils = require("dubzland.utils")
 local fugitive_config = {}
 
 fugitive_config.init = function()
-    utils.keys.nnoremap("<leader>gs", vim.cmd.Git, "Show git status window")
-    utils.keys.nnoremap("<leader>gc", function() vim.cmd.Git("commit") end, "Open the git commit window")
+    local fugitive_prefix = "<leader>g"
+    utils.keys.nnoremap_prefix(fugitive_prefix, "s", vim.cmd.Git, "Show git status window")
+    utils.keys.nnoremap_prefix(fugitive_prefix, "c", function() vim.cmd.Git("commit") end, "Open the git commit window")
+    utils.keys.nnoremap_prefix(fugitive_prefix, "v", vim.cmd.Gvdiffsplit, "Open a veritical diff")
 end
 
 return fugitive_config

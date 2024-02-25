5180dda03f3672af80e478e87f2b821563ca43b8
Josh Williams <jdubz@dubzland.com>
2024-02-25 10:23:19 -0600

    Text manipulation keymaps

--- .config/nvim/lua/dubzland/keymaps.lua
+++ .config/nvim/lua/dubzland/keymaps.lua
@@ -3,17 +3,6 @@ local utils = require("dubzland.utils")
 local M = {}
 
 M.init = function()
-    utils.keys.nnoremap("<leader>pv", vim.cmd.Ex)
-    -- {{{ Movement
-    utils.keys.nnoremap("<C-h>", "<C-w>h", "Move cursor one buffer to the left")
-    utils.keys.nnoremap("<C-h>", "<C-w>h", "Move cursor one buffer to the left")
-    utils.keys.nnoremap("<C-j>", "<C-w>j", "Move cursor one buffer down")
-    utils.keys.nnoremap("<C-k>", "<C-w>k", "Move cursor one buffer up")
-    utils.keys.nnoremap("<C-l>", "<C-w>l", "Move cursor one buffer to the right")
-    utils.keys.vnoremap("K", ":m '<-2<CR>gv=gv", "Move selected text up one line")
-    utils.keys.vnoremap("J", ":m '>+1<CR>gv=gv", "Move selected text down one line")
-    -- }}}
-
     -- {{{ Search
     utils.keys.nnoremap("<leader><space>", function()
         vim.cmd("noh")
@@ -25,9 +14,33 @@ M.init = function()
     utils.keys.vnoremap("/", "/\\v")
     -- }}}
 
+    -- {{{ File explorer
+    utils.keys.nnoremap("<leader>pv", vim.cmd.Ex)
+    -- }}}
+
+    -- {{{ Movement
+    utils.keys.nnoremap("<C-h>", "<C-w>h", "Move cursor one buffer to the left")
+    utils.keys.nnoremap("<C-h>", "<C-w>h", "Move cursor one buffer to the left")
+    utils.keys.nnoremap("<C-j>", "<C-w>j", "Move cursor one buffer down")
+    utils.keys.nnoremap("<C-k>", "<C-w>k", "Move cursor one buffer up")
+    utils.keys.nnoremap("<C-l>", "<C-w>l", "Move cursor one buffer to the right")
+    -- }}}
+
+    -- {{{ Text manipulation
+    utils.keys.vnoremap("K", ":m '<-2<CR>gv=gv", "Move selected text up one line")
+    utils.keys.vnoremap("J", ":m '>+1<CR>gv=gv", "Move selected text down one line")
+
+    utils.keys.noremap({ "n", "v" }, "<leader>y", [["+y]], "Same as y, but to the system clipboard")
+    utils.keys.noremap({ "n", "v" }, "<leader>Y", [["+Y]], "Same as Y, but to the system clipboard")
+
+    utils.keys.vnoremap("<leader>p", [["_dP]], "Paste over text without losing register")
+    -- }}}
+
     -- {{{ Window manipulation
-    utils.keys.nnoremap("<leader>q", utils.delete_buffer_keep_window,
+    utils.keys.nnoremap("<leader>qk", utils.delete_buffer_keep_window,
         "Close the current buffer, but keep the window open")
+    utils.keys.nnoremap("<leader>qq", function() vim.api.nvim_command("bwipeout") end,
+        "Close the current buffer, and close the window")
     utils.keys.nmap("<leader>wco", function() vim.cmd("copen") end, "Open the quickfix window")
     utils.keys.nmap("<leader>wcc", function() vim.cmd("cclose") end, "Close the quickfix window")
     utils.keys.nmap("<leader>wlo", function() vim.cmd("lopen") end, "Open the location window")

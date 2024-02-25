067ef312c2fd50fcaa8101a177bb8ef70e916a60
Josh Williams <jdubz@dubzland.com>
2024-02-25 13:10:31 -0600

    Add git push mapping

--- .config/nvim/lua/dubzland/config/vim-fugitive.lua
+++ .config/nvim/lua/dubzland/config/vim-fugitive.lua
@@ -7,6 +7,10 @@ fugitive_config.init = function()
     utils.keys.nnoremap_prefix(fugitive_prefix, "s", vim.cmd.Git, "Show git status window")
     utils.keys.nnoremap_prefix(fugitive_prefix, "c", function() vim.cmd.Git("commit") end, "Open the git commit window")
     utils.keys.nnoremap_prefix(fugitive_prefix, "v", vim.cmd.Gvdiffsplit, "Open a veritical diff")
+    utils.keys.nnoremap_prefix(fugitive_prefix, "p", function()
+        local branch = vim.fn.FugitiveHead()
+        vim.cmd.Git("push origin " .. branch)
+    end, "Perform a git push")
 end
 
 return fugitive_config

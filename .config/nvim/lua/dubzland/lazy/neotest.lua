8e899f329fa0064fecffd20e7e02d437054491e0
Josh Williams <jdubz@dubzland.com>
2024-04-30 07:21:55 -0500

    Add nvim-nio for neotest

--- .config/nvim/lua/dubzland/lazy/neotest.lua
+++ .config/nvim/lua/dubzland/lazy/neotest.lua
@@ -9,6 +9,7 @@ return {
         'nvim-neotest/neotest-plenary',
         'nvim-neotest/neotest-python',
         'nvim-neotest/neotest-vim-test',
+        'nvim-neotest/nvim-nio',
         'folke/trouble.nvim',
     },
     config = function(_, opts)

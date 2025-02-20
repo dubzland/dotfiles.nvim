0c9cf458d35083e44e11e49964c12c34437ae2d1
Josh Williams <jdubz@dubzland.com>
2025-02-20 15:40:51 -0600

    Use black for null-ls formatting

--- .config/nvim/lua/dubzland/config/mason-null-ls.lua
+++ .config/nvim/lua/dubzland/config/mason-null-ls.lua
@@ -7,6 +7,12 @@ mason_null_ls_config.init = function(opts)
     require("mason-null-ls").setup(utils.merge({
         ensure_installed = { "black", "prettier" },
     }, opts))
+    local null_ls = require("null-ls")
+    null_ls.setup({
+        sources = {
+            null_ls.builtins.formatting.black
+        }
+    })
 end
 
 return mason_null_ls_config
